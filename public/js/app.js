23dab80d8ba3feab4d5e9d28c7647a60b01292fa
Pierrick Varin <pierrick.varin@glitchstudio.com>
2016-08-23 19:55:03 +0200

    Started Edition of dashboard entries

--- public/js/app.js
+++ public/js/app.js
@@ -52,4 +52,42 @@ $( function() {
 
 		return false;
 	});
+
+	/* Ajax edit budget entries on Dashboard */
+	$('.dashboard-budget-table').on('click', '.edit-entry', function(e) {
+		e.preventDefault();
+
+		var $this = $(this);
+		var $tr = $this.closest('tr');
+
+		var tr_id = $tr.attr('data-line-id');
+		var $editor = $('.dashboard-budget-table tr.edit-line-template[data-line-id='+tr_id+']');
+
+		$tr.hide();
+		$editor.show();
+		// $.post('/ajax_edit_entry', {entry_id: $(this).attr('data-entry-id')}, function(data) {
+		// 	if( data.status == "ok" ) {
+				
+		// 		var visible_update = data.visible_account_update;
+		// 		$('.visible-account-update').html(data.visible_account_update + ' €');
+		// 		if( data.visible_account_update > 0 ) {
+		// 			$('.visible-account-update').removeClass('text-danger').addClass('text-success');
+		// 		} else {
+		// 			$('.visible-account-update').removeClass('text-success').addClass('text-danger');
+		// 		}
+
+		// 		var real_update = data.real_account_update;
+		// 		$('.real-account-update').html(data.real_account_update + ' €');
+		// 		if( data.real_account_update > 0 ) {
+		// 			$('.real-account-update').removeClass('text-danger').addClass('text-success');
+		// 		} else {
+		// 			$('.real-account-update').removeClass('text-success').addClass('text-danger');
+		// 		}
+
+		// 		$this.closest('tr').remove();
+		// 	}
+		// }, 'json');
+
+		return false;
+	});
 });
